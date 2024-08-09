d5e123192715bb3ac2ef4629c855801cb6773fcd
Shrinivasofficial <shrinivasoffical@gmail.com>
2024-08-09 21:13:31 +0530

    api integrated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar'; // No curly braces needed
 import Hero from './Components/Hero'; // No curly braces needed
 import Chat from './Components/Chat'; // No curly braces needed
+import { Footer } from './Components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Hero />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
+      <Footer></Footer>
     </Router>
   );
 }
